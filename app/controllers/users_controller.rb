b4d4854e384db40eaea522b2ade06ea3b9e08ac0
daisuketakahashi7 <d.takahashitakahashi@gmail.com>
2022-02-11 15:43:31 +0000

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,30 @@
 class UsersController < ApplicationController
-  def new
+  
+  def show
+    @user = User.find(params[:id])
   end
+  
+  def new
+    #newアクションに@user変数を追加する
+    @user = User.new
+  end
+  
+  def create
+      # createアクションでStrong Parametersを使う
+    @user = User.new(user_params)
+    if @user.save
+      # 保存の成功をここで扱う
+      flash[:success] = "Welcome to the Sample App!"     # ユーザー登録ページにフラッシュメッセージを追加する
+      redirect_to @user      # 保存とリダイレクトを行う、userのcreateアクション
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
+                                   :password_confirmation)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,4 +7,6 @@ Rails.application.routes.draw do
   get  '/about',   to: 'static_pages#about'
   get  '/contact', to: 'static_pages#contact'
   get  '/signup',  to: 'users#new'
+  #/users/1 のURLを有効にする
+  resources :users
 end
